2c6189f961b5f366fb91cc828a7552698df8a93f
Mateo Cubertie <mcubertie@gmail.com>
2024-09-11 21:46:50 -0300

    config_web_0.1

--- esp32_main.ino
+++ esp32_main.ino
@@ -1,96 +1,102 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <NetworkClient.h>
+#include <WiFiAP.h>
 
-const char *ssid = "Laura1";
-const char *password = "20598996";
-
+const char *ssid = "esp32ap";
+const char *password = "microesp32";
+IPAddress espIP;
+NetworkServer server(80);
 void setup() {
   Serial.begin(115200);
-  delay(10);
-
-
-  // We start by connecting to a WiFi network
-  // To debug, please enable Core Debug Level to Verbose
-
   Serial.println();
-  Serial.print("[WiFi] Connecting to ");
-  Serial.println(ssid);
+  Serial.println("Configurando Access Point...");
 
-  WiFi.begin(ssid, password);
-  // Auto reconnect is set true as default
-  // To set auto connect off, use the following function
-  //    WiFi.setAutoReconnect(false);
-
-  // Will try for about 10 seconds (20x 500ms)
-  int tryDelay = 500;
-  int numberOfTries = 20;
-
-  // Wait for the WiFi event
-  while (true) {
-
-    switch (WiFi.status()) {
-      case WL_NO_SSID_AVAIL: Serial.println("[WiFi] SSID not found"); break;
-      case WL_CONNECT_FAILED:
-        Serial.print("[WiFi] Failed - WiFi not connected! Reason: ");
-        return;
-        break;
-      case WL_CONNECTION_LOST: Serial.println("[WiFi] Connection was lost"); break;
-      case WL_SCAN_COMPLETED:  Serial.println("[WiFi] Scan is completed"); break;
-      case WL_DISCONNECTED:    Serial.println("[WiFi] WiFi is disconnected"); break;
-      case WL_CONNECTED:
-        Serial.println("[WiFi] WiFi is connected!");
-        Serial.print("[WiFi] IP address: ");
-        Serial.println(WiFi.localIP());
-        return;
-        break;
-      default:
-        Serial.print("[WiFi] WiFi Status: ");
-        Serial.println(WiFi.status());
-        break;
-    }
-    delay(tryDelay);
-
-    if (numberOfTries <= 0) {
-      Serial.print("[WiFi] Failed to connect to WiFi!");
-      // Use disconnect function to force stop trying to connect
-      WiFi.disconnect();
-      return;
-    } else {
-      numberOfTries--;
-    }
+  if (!WiFi.softAP(ssid, password)) {
+    Serial.println("Error al iniciar el Access Point");
+    while (1)
+      ;
   }
+
+  espIP = WiFi.softAPIP();
+  Serial.print("IP del Access Point: ");
+  Serial.println(espIP);
+  server.begin();
+
+  Serial.println("Servidor iniciado");
 }
 
 void loop() {
-//Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
-      HTTPClient http;
+  NetworkClient client = server.accept();  // listen for incoming clients
 
-      String serverPath = "http://192.168.100.15:80/update?temp=80";
-      
-      // Your Domain name with URL path or IP address with path
-      http.begin(serverPath.c_str());
-      
-      
-      // Send HTTP GET request
-      int httpResponseCode = http.GET();
-      
-      if (httpResponseCode>0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        String payload = http.getString();
-        Serial.println(payload);
+  if (client) {                           // if you get a client,
+    Serial.println("Cliente conectado");  // print a message out the serial port
+    String currentLine = "";              // make a String to hold incoming data from the client
+    while (client.connected()) {          // loop while the client's connected
+      if (client.available()) {           // if there's bytes to read from the client,
+        char c = client.read();           // read a byte, then
+        Serial.write(c);                  // print it out the serial monitor
+        if (c == '\n') {                  // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            printWebHTML(client);
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {  // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
       }
-      else {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
-      }
-      // Free resources
-      http.end();
     }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
-    delay(5000);
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
+}
+
+// Funcion que manda al cliente las lineas del HTML de la web
+// (escribir el codigo en VSC y pasarlo, pero hay que generar otro print donde hay variables)
+// Las comillas dobles hay que bypassearlas con '\' (buscar y reemplazar en seleccion)
+void printWebHTML(NetworkClient client) {
+  // the content of the HTTP response follows the header:
+//   client.print("<!DOCTYPE html> <html lang=\"en\"><head>");
+//   client.print("<meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+//   client.print("<title>Configuracion | Enlace Inalambrico</title><style>");
+//   client.print(" @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');");
+//   client.print("html { background-color: rgb(18, 18, 18); color: white; font-family: \"Roboto\", sans-serif; }");
+//   client.print(".cfgPrincipal{ display: flex; flex-direction: column;justify-content: center; }");
+//   client.print(".resaltado{color:rgb(0, 230, 0)}");
+//   client.print("</style></head><body><section class=\"cfgPrincipal\">");
+//   client.print("<h1>Configuracion | Enlace inalámbrico</h1>");
+//   client.print("<h2>IP Actual: <span class=\"resaltado\"></span></h2>");
+//   client.print("<h3>");
+  client.print("<!DOCTYPE html> <html lang=\"en\"><head> \
+    <meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> \
+    <title>Configuracion | Enlace Inalambrico</title><style> \
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'); \
+    html { background-color: rgb(18, 18, 18); color: white; font-family: \"Roboto\", sans-serif; } \
+    .cfgPrincipal{ display: flex; flex-direction: column;align-items: center; } \
+    .resaltado{ color:rgb(0, 230, 0);} \
+    </style></head><body><section class=\"cfgPrincipal\"> \
+    <h1>Configuracion | Enlace inalámbrico</h1> \
+    <h2>IP Actual: <span class=\"resaltado\"> \
+    <h3>"); 
+  client.print(espIP);
+  client.print("</span></h2></h3></section></body></html>");
+    // client.print("</h3>");
+    // client.print("</section></body></html>");
 }
 
